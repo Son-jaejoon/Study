46afc7415c8f7062beea6e7ad678c78e996045b6
손재준 <jaejoon.programmer@gmail.com>
2023-05-16 15:51:22 +0900

    얕은 복사의 문제점

--- Cpp/이것이 C++이다/4장 스크립트 공부/BadShallowCopy.cpp
+++ Cpp/이것이 C++이다/4장 스크립트 공부/BadShallowCopy.cpp
@@ -11,11 +11,11 @@ int main()
     *pA = 10;
 
     pB = new int;
-    pB = pA;
-
+    pB = pA; // 기존에 가리키던 int 인스턴스를 방치하고 pA의 인스턴스를 가리킴
+             // 이후 접근 불가능한 int 인스턴스가 발생함
     cout << *pA << endl;
     cout << *pB << endl;
 
     delete pA;
-    delete pB;
+    delete pB; // 이미 해제된 int 인스턴스를 해제하려함.
 }

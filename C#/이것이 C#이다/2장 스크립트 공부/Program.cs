2fcfe05937fc65c9df34cea0e26768a4f896a7ac
손재준 <jaejoon.programmer@gmail.com>
2023-06-06 18:14:14 +0900

    기본 프로그램

---
+++ C#/이것이 C#이다/2장 스크립트 공부/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : 2장 스크립트 공부 <이름> ");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
